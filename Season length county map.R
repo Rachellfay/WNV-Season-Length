53cc026e8444fd2d53cebd64d4c75f828cc887b0
Rachel Fay <95448026+Rachellfay@users.noreply.github.com>
2026-01-29 17:34:58 -0800

    Update Season length county map.R

--- Season length county map.R
+++ Season length county map.R
@@ -11,24 +11,24 @@
 # set working directory
 setwd("~/Desktop/rachelWNV")
 
-##--------- Set data and pacakges up 
+## Set data and pacakges up ## 
 
-# packages
+# packages ##
 library(tidyverse); library(readxl); library(data.table); library(dplyr); library(openxlsx) 
 
-# data
+# data ##
 data <- read_excel("NewYork_1999-2024_long.xlsx")
 
 head(data)
 
-# convert to data.table for efficient processing
+# convert to data.table for efficient processing ##
 setDT(data)
 
-# filter days over 16.7
+# filter days over 16.7 ##
 over_16 <- data[tmeanc >= 16.7]
 
 # identify the first and last valid occurrences: 
-# based on the first and last time two days above 16.7 occur within 13 days of each other
+# based on the first and last time two days above 16.7 occur within 13 days of each other ##
 valid_dates <- over_16[order(district, year, doy)][ # first make sure data is ordered by county, year, date of year
   , `:=`(
     dnext = shift(doy, type = "lead") - doy, # calculate the number of days between the next day it was 16.7
@@ -44,13 +44,13 @@ valid_dates <- over_16[order(district, year, doy)][ # first make sure data is or
 ][
   , DaysBetween := LastValid - FirstValid]
 
-# quick look at result summary
+# quick look at result summary ##
 mean(valid_dates$FirstValid) # 121.4882
 mean(valid_dates$LastValid) # 282.531
 
 write.xlsx(valid_dates, 'wnv_transmission_season_1999_2024_12.1.25.xlsx', rowNames = FALSE)
 
-## ---------- Calculating change in the season length per county using linear regression
+## Calculating change in the season length per county using linear regression ##
 season_change_lm <- c()
 
 for(d in unique(valid_dates$district)){
@@ -85,7 +85,7 @@ mean(season_change_lm$se_change_in_length) # 10.59665
 
 write.xlsx(season_change_lm, 'wnv_transmission_season_averages_lm_test_new.xlsx', rowNames = FALSE)  
 
-## ---------- Calculating change in the first day per county using linear regression
+## Calculating change in the first day per county using linear regression ##
 season_first_lm <- data.frame()
 
 for (d in unique(valid_dates$district)) {
@@ -94,7 +94,7 @@ for (d in unique(valid_dates$district)) {
     dplyr::arrange(year)
   
   if (nrow(district_data) < 2 || length(unique(district_data$year)) < 2) {
-    # not enough data to fit a slope
+    # not enough data to fit a slope ##
     estimate_df <- data.frame(
       district = d,
       avg_first_1999 = NA_real_,
@@ -130,15 +130,15 @@ for (d in unique(valid_dates$district)) {
   season_first_lm <- rbind(season_first_lm, estimate_df)
 }
 
-# quick view of results
+# quick view of results ##
 hist(season_first_lm$change_in_first)
 mean(season_first_lm$change_in_first, na.rm = TRUE) # -3.782327
 mean(season_first_lm$se_change_in_first, na.rm = TRUE) # 8.602073
 
-## save
+## save ##
 write.xlsx(season_first_lm, 'season_first_day_regression_lm.xlsx')
 
-##--------- Calculating change in the last day per county using linear regression
+## Calculating change in the last day per county using linear regression ##
 season_last_lm <- data.frame()
 
 for (d in unique(valid_dates$district)) {
@@ -182,37 +182,37 @@ for (d in unique(valid_dates$district)) {
   season_last_lm <- rbind(season_last_lm, estimate_df)
 }
 
-# quick view of results
+# quick view of results ##
 hist(season_last_lm$change_in_last)
 mean(season_last_lm$change_in_last, na.rm = TRUE) # 16.275443
 mean(season_last_lm$se_change_in_last, na.rm = TRUE) # 5.552685
 
 
-## save
+## save ##
 write.xlsx(season_last_lm, 'season_last_day_regression_lm.xlsx')
 
-#### make maps #########################
+#### make maps ####
 ###fig 2B###
-# set working directory
+# set working directory #
 setwd("~/Desktop/rachelWNV")
 
-# install.packages
+# install.packages #
 library(ggplot2)
 library(sf)
 library(dplyr)
 library(readxl)
 library(openxlsx) 
 
-# Import a geojson or shapefile
+# Import a geojson or shapefile #
 map <- read_sf("Counties.shp")
 colnames(map)
 
-## state shape
+## state shape ##
 ggplot(map) +
   geom_sf() 
 
-### anomaly in first day
-# read in data
+### anomaly in first day ###
+# read in data #
 anomaly_df <-read_excel("season_first_day_regression_lm.xlsx")
 
 View(anomaly_df)
@@ -225,10 +225,10 @@ anomaly_shape <- anomaly_df |> left_join(anomaly_df, by = "NAME")
 
 print(anomaly_shape)
 
-## merge anomaly10
+## merge anomaly10 ##
 map$data <- anomaly_shape$change_in_first.x
 
-## plot
+## plot ##
 ggplot(map) + 
   geom_sf(aes(fill = data)) +  
   scale_fill_gradientn(colors = c("purple3", "lightpink", "white","lightgoldenrodyellow"  ),
@@ -236,40 +236,40 @@ ggplot(map) +
                        limits = c(-20, 10)) +  
   theme_classic()
 
-## chanage in last day  fig 2D##################
-# install.packages
+## chanage in last day  fig 2D##
+# install.packages #
 library(ggplot2)
 library(sf)
 library(dplyr)
 library(readxl)
 
-# Import a geojson or shapefile
+# Import a geojson or shapefile #
 map <- read_sf("Counties.shp")
 colnames(map)
 
-## state shape
+## state shape ##
 ggplot(map) +
   geom_sf() 
 
 
-### anomaly in first day
-# read in data
+### anomaly in first day ###
+# read in data #
 anomaly_df <-read_excel("season_last_day_regression_lm.xlsx")
 
 View(anomaly_df)
 
-#merge data and shapefile
+#merge data and shapefile #
 anomaly_df <- anomaly_df |> rename(NAME = district)
 
-#merge
+#merge #
 anomaly_shape <- anomaly_df |> left_join(anomaly_df, by = "NAME")
 
 print(anomaly_shape)
 
-# merge anomaly10
+# merge anomaly10 #
 map$data <- anomaly_shape$change_in_last.x
 
-##plot
+##plot ##
 ggplot(map) + 
  geom_sf(aes(fill = data), color = "black") +  # Ensure fill uses the merged column
  scale_fill_gradientn(colors = c("bisque", "white", "lightgreen", "darkgreen", "#014122"), 
@@ -277,9 +277,9 @@ ggplot(map) +
                      limits = c(-10, 30)) + 
   theme_classic()
 
-### map for change in season length map fig 1C
+### map for change in season length map fig 1C ###
 
-# set working directory
+# set working directory #
 setwd("~/Desktop/rachelWNV")
 
 library(sf)
@@ -288,21 +288,21 @@ library(dplyr)
 library(readxl)
 library(openxlsx) 
 
-# Import a geojson or shapefile ### season length change
+# Import a geojson or shapefile ### season length change #
 map <- read_sf("Counties.shp")
 colnames(map)
 
-## state shape
+## state shape ##
 ggplot(map) +
   geom_sf() 
 
 
-# Read only the columns you need from Excel
+# Read only the columns you need from Excel #
 anomaly_df <- read_excel("wnv_transmission_season_averages_lm_test_new.xlsx") %>%
   select(district, change_in_length) %>%   # keep only district and change_in_length
   rename(NAME = district)                  # match shapefile column
 
-#merge data and shapefile
+# merge data and shapefile #
 anomaly_df <- anomaly_df |> rename(NAME = district)
 
 #merge
@@ -310,10 +310,10 @@ anomaly_shape <- anomaly_df |> left_join(anomaly_df, by = "NAME")
 
 print(anomaly_shape)
 
-# merge anomaly10
+# merge anomaly10 #
 map$data <- anomaly_shape$change_in_length.x
 
-#plot map and change theme
+# plot map and change theme #
 ggplot(map) + 
   geom_sf(aes(fill = data), color = "black") +  # Ensure fill uses the merged column
   scale_fill_gradientn(colors = c("#7588A1", "white","#CCCCCC", "#999999", "#666666", "#333333", "#000000"), 
